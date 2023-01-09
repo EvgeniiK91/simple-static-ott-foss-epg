9f20969f117ef08aa65fb67c1ea7a994066c1ad2
EvgeniiK91 <107505718+EvgeniiK91@users.noreply.github.com>
2023-01-09 17:47:26 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CONV_BIN="ott-play-epg-converter"
-LATEST_RELEASE=`curl -s https://api.github.com/repos/prog4food/ott-play-epg-converter/releases/88104667 | sed -nE 's/"browser_download_url": "([^"]+)"/\1/p' | grep "_linux_x64\.gz$"`
-[ -z $88104667_RELEASE ] && { echo "empty 88104667 release"; exit 1; }
-curl -Ls $88104667_RELEASE | gzip -d > $CONV_BIN
+LATEST_RELEASE=`curl -s https://api.github.com/repos/prog4food/ott-play-epg-converter/releases/latest | sed -nE 's/"browser_download_url": "([^"]+)"/\1/p' | grep "ofoss-epg-conv_linux_x64\.gz$"`
+[ -z $LATEST_RELEASE ] && { echo "empty latest release"; exit 1; }
+curl -Ls $LATEST_RELEASE | gzip -d > $CONV_BIN
 [ -s $CONV_BIN ] || { echo "bad converter release"; exit 1; }
 
 chmod +x $CONV_BIN
